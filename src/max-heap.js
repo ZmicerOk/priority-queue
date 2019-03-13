0134f800441c9e45db79350f5b5d7d9760bdcf3b
ZmicerOk <vasikou73@gmail.com>
2019-03-14 00:42:30 +0300

    feat: code in progress

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		var node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
@@ -34,7 +37,7 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if (heap == 0){this.root=passed};
 	}
 
 	shiftNodeUp(node) {
